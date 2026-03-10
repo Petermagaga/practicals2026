b7e7a7be7174b19c93b4c417d8f29c430c9e4c93
petermagaga <ptrmagaga@gmail.com>
2026-03-10 11:53:02 +0300

    iss_position

--- password gen/api_end_points.py
+++ password gen/api_end_points.py
@@ -1,23 +1,25 @@
 MY_LAT=-1.478911
 MY_LONG=36.956780
 import datetime
-"""import requests
-
-response=requests.get(url="http://api.open-notify.org/iss-now.json")
-
-
-response.raise_for_status()
-
-data=response.json()
-print(data['iss_position'])
-longitude=(data['iss_position']['longitude'])
-latitude=(data['iss_position']['latitude'])
-
-iss_position=(longitude,latitude)
-print(iss_position)"""
-
-
 import requests
+
+def is_iss_overhead():
+    response=requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data=response.json()
+    print(data['iss_position'])
+    iss_longitude=float((data['iss_position']['longitude']))
+    iss_latitude=float((data['iss_position']['latitude']))
+
+    iss_position=(iss_longitude,iss_latitude)
+
+    print(iss_position)
+
+    if MY_LONG-5<=iss_longitude<=MY_LONG+5 and MY_LAT-5<=iss_latitude<=MY_LAT+5:
+        return True
+
+
+
 
 parameters= {
     "lat":MY_LAT,
@@ -40,4 +42,5 @@ print(f"sunrise {hrs_sunrise}, \n sunset {hrs_sunset}")
 
 time_now =datetime.datetime.now().hour
 print(hrs_sunset-time_now)
+
 
